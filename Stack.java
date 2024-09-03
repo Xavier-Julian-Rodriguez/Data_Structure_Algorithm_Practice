67944af50b4ba33e11feebdbcafbc2bc03790444
Xavier-Julian-Rodriguez <xavierjulianrod@gmail.com>
2024-09-03 16:14:31 -0400

    adds popNode method

--- Stack.java
+++ Stack.java
@@ -43,7 +43,18 @@ public class Stack {
             top = newNode;
         }
         height++;
-        
     }
-    
+
+    public Node popNode() {
+        
+        if(height == 0) {
+            return null;
+        }
+        Node temp = top;
+        top = top.next;
+        temp.next = null;
+        height--;
+        
+        return temp;
+    }
 }
